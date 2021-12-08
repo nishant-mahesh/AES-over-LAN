07ff9982529f2862168ac12b3aff15de81b57102
Nishant Mahesh <72411696+nishant-mahesh@users.noreply.github.com>
2021-12-08 16:43:55 +0530

    Final Commit | Added Comments and Tests

---
+++ client.py
@@ -0,0 +1,131 @@
+'''
+Name 		: Nishant Mahesh, Neil Chowdhary
+Code 		: Course Project CS 1340
+Description	: 	- In this project, we will implement a simple AES encryption/ decryption application using socket programming.
+				- Upon establishing server and client connection, client can choose to encrypt/ decrypt a message.
+				- Client then sends the plaintext (or ciphertext) and a valid 16 byte key to the server
+				- Server performs the encryption (or decryption) and sends the resulting ciphertext (or plaintext) to client
+				- Client displays the result and disconnects
+
+Testing 	:In order to test the project, the evaluator can do one of 2 things.
+				a) 	Run both client and server on the same machine (locally), using the local IP 127.0.0.1 and PORT 5545
+
+				b) 	Run server on one machine, and client on another machine (LAN implementation). 
+					For this to work, ensure the IP and PORT on the server side are set using the "FOR LAN" portion of the code.
+					After starting the server, you can see the IP and PORT that the server is hosted on. Copy this over to the 
+					client side and connect to the correct IP and PORT, and run the command line application over LAN
+
+			Some test cases to run encryption/ decryption on are as follows:
+
+				1. 	Test Msg  		: this is a test message
+					Test Key  		: hdgfetwyusiwacdf
+					Encrypted hex   : 9d12b3b0ed25970fbe4fad7772a827b83d6f96c8696cd6ef1a34f9fc381f72f2
+
+
+				2.	Test Msg  		: networks
+					Test Key  		: asdfasdfasdfasdf
+					Encrypted hex  	: 941870dfe2ed6b14e56409dd1d17abbb
+'''				
+
+import socket
+import json
+import pickle
+
+
+#================Choosing IP and PORT================#
+'''
+For testing LOCALLY, use (IP = '127.0.0.1' , PORT = 5545)
+For testing on LAN, use (IP = server's IP, PORT = 10000)
+'''
+
+IP   = '127.0.0.1' # Change from 127.0.0.1 to server's IP if testing on LAN
+PORT = 5545
+
+#================Setting up client================#
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to the server
+clientsocket.connect((IP, PORT))
+
+
+#================Client-Server Interaction================#
+
+# printing intro message
+print()
+print('---------------------------------------')
+print(clientsocket.recv(1024).decode()) #receive intro message
+print('---------------------------------------')
+print()
+
+# sending OK response to ensure connection is successful, and interaction can begin
+clientsocket.send('OK'.encode())
+
+choiceMessage = clientsocket.recv(1024).decode() #receive prompt for encryption or decryption
+
+choice = input(choiceMessage)
+choice = choice.lower()
+
+while(choice != 'e' and choice != 'd'): # ensure correct input is sent to the server
+	print("Incorrect choice! Please re-enter")
+	print()
+	choice = input(choiceMessage)
+	choice = choice.lower()
+
+clientsocket.send(choice.encode()) # send client's choice of encryption/decryption to the server
+
+# Client chooses ENCRYPTION
+if(choice == 'e'):
+
+	messageRequest = clientsocket.recv(1024).decode() # receive prompt for plaintext from server
+	plaintext = input(messageRequest) # input plaintext from the client
+
+	clientsocket.send(plaintext.encode()) # send the plaintext to server
+
+	keyRequest = clientsocket.recv(1024).decode() # receive prompt for secret key from server
+	key = input(keyRequest) # # input secret key from the client
+
+
+	while(len(key) != 16): # ensure secret key is 16 bytes (required for AES)
+		print("Key length is not 128 bits! Please re-enter")
+		print()
+		key = input(keyRequest)
+
+	clientsocket.send(key.encode()) # send key to server
+
+	ciphertext = clientsocket.recv(1024) # receive ciphertext from server in bytearray fromat
+
+	print(f"The ciphertext in hex is: {ciphertext.hex()}") # convert bytearray string to hex string and print
+	print()
+
+
+# Client chooses DECRYPTION
+else:
+
+	messageRequest = clientsocket.recv(1024).decode() # receive prompt for ciphertext from server
+	ciphertext = input(messageRequest) # input ciphertext from the client
+
+	clientsocket.send(ciphertext.encode()) # send the ciphertext to server
+
+	keyRequest = clientsocket.recv(1024).decode() # receive prompt for secret key from server
+	key = input(keyRequest) # input secret key from the client
+
+
+	while(len(key) != 16): # ensure secret key is 16 bytes (required for AES)
+		print("Key length is not 128 bits! Please re-enter")
+		print()
+		key = input(keyRequest)
+
+	clientsocket.send(key.encode()) # send valid key to server
+
+	data = json.loads(clientsocket.recv(1024).decode()) # receive decrypted message (or error message) from server
+	
+	# If error occured during decryption
+	if(data[0] == "FAIL"):
+		print(data[1]) #print message for client
+
+	# If decryption was successful
+	else:
+		assert(data[0] == "SUCCESS")
+		print(f"The plaintext is: {data[1]}") #print message for client
+	print()
+
---
+++ server.py
@@ -0,0 +1,210 @@
+'''
+Name 		: Nishant Mahesh, Neil Chowdhary
+Code 		: Course Project CS 1340
+Description	: 	- In this project, we will implement a simple AES encryption/ decryption application using socket programming.
+				- Upon establishing server and client connection, client can choose to encrypt/ decrypt a message.
+				- Client then sends the plaintext (or ciphertext) and a valid 16 byte key to the server
+				- Server performs the encryption (or decryption) and sends the resulting ciphertext (or plaintext) to client
+				- Client displays the result and disconnects
+
+Testing 	:In order to test the project, the evaluator can do one of 2 things.
+				a) 	Run both client and server on the same machine (locally), using the local IP 127.0.0.1 and PORT 5545
+
+				b) 	Run server on one machine, and client on another machine (LAN implementation). 
+					For this to work, ensure the IP and PORT on the server side are set using the "FOR LAN" portion of the code.
+					After starting the server, you can see the IP and PORT that the server is hosted on. Copy this over to the 
+					client side and connect to the correct IP and PORT, and run the command line application over LAN
+
+			Some test cases to run encryption/ decryption on are as follows:
+
+				1. 	Test Msg  		: this is a test message
+					Test Key  		: hdgfetwyusiwacdf
+					Encrypted hex   : 9d12b3b0ed25970fbe4fad7772a827b83d6f96c8696cd6ef1a34f9fc381f72f2
+
+
+				2.	Test Msg  		: networks
+					Test Key  		: asdfasdfasdfasdf
+					Encrypted hex  	: 941870dfe2ed6b14e56409dd1d17abbb
+
+'''				
+
+import socket
+import json
+import string
+import random
+from os import urandom
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
+
+#================Choosing IP and PORT================#
+
+
+# For testing LOCALLY (not LAN), use the IP and PORT below
+
+# ----- FOR LOCAL ----- #
+IP   = '127.0.0.1'
+PORT = 5545
+
+
+# For testing on LAN, use the code section below
+
+# ------- FOR LAN ------ #
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s.connect(("8.8.8.8", 80))
+# IP = s.getsockname()[0]
+# s.close()
+# PORT = 10000
+
+
+#================Setting up server================#
+
+#server socket instantiation
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind server
+serversocket.bind((IP, PORT))
+
+# start listening
+serversocket.listen()
+
+print(f'Server is running! Listening for transmission from client at: ({IP}, {PORT})')
+print()
+
+#================Server-Client Interaction================#
+
+# Default initialisation vector
+iv = b'ghidhjuwvxgshwak' 
+
+
+def handle_client(clientsocket, address):
+	global iv
+	print(f'New connection made with client at address {address}') 
+
+	introMessage = 'Welcome to the AES encryption/ decryption portal!'
+
+	clientsocket.send(introMessage.encode()) #send introductory message to client
+
+	if(clientsocket.recv(1024).decode() != 'OK'):
+		print('Connection failed! Disconnecting...')
+		clientsocket.close()
+
+	choiceMessage = 'Do you want to encrypt or decrypt? (Enter E/D): '	
+	clientsocket.send(choiceMessage.encode()) #Ask client to choose between encryption and decryption
+
+
+	choice = (clientsocket.recv(1024).decode()).lower()
+
+	assert(choice == 'e' or choice == 'd')
+
+	# Client chooses ENCRYPTION
+	if(choice == 'e'):
+
+		messageRequest = 'Enter the plaintext: '
+		clientsocket.send(messageRequest.encode()) # prompt client for the plaintext
+
+		plaintext = clientsocket.recv(1024).decode() # receive the plaintext from client
+
+
+		keyRequest= 'Enter the secret key: ' 
+		clientsocket.send(keyRequest.encode()) # prompt client for the key
+
+		key = clientsocket.recv(1024).decode() # receive the key from client
+
+		assert(len(key) == 16)
+
+		plaintext = bytearray(plaintext.encode()) # convert plaintext from string to bytearray for encryption operation
+
+		paddedPt = pad(plaintext, AES.block_size) # pad the bytearray to ensure it is a multiple of 16
+
+		key = bytearray(key.encode()) # convert key from string to bytearray for encryption operation
+
+		aes_obj = AES.new(key, AES.MODE_CBC, iv) 
+
+		ciphertext = aes_obj.encrypt(paddedPt) #produce encrypted bytearray
+
+		clientsocket.send(ciphertext) #send bytearray string to client to convert to hex on client side
+
+	# Client chooses DECRYPTION
+	else:
+		messageRequest = 'Enter the ciphertext (in hex): '
+		clientsocket.send(messageRequest.encode()) # prompt client for the ciphertext (in hex format)
+
+		ciphertext = clientsocket.recv(1024).decode() # receive the ciphertext from client
+
+
+		keyRequest= 'Enter the secret key: '
+		clientsocket.send(keyRequest.encode()) # prompt client for the key
+
+		key = clientsocket.recv(1024).decode() # receive the key from client
+
+		ciphertext = bytes.fromhex(ciphertext) # convert hex cipher to byte format
+
+		key = bytearray(key.encode()) # convert to bytearrray for decryption
+
+		try: 
+
+			aes_obj = AES.new(key, AES.MODE_CBC, iv) 
+			
+			plaintext = aes_obj.decrypt(ciphertext) # decrypt the ciphertext
+			
+			plaintext = unpad(plaintext, AES.block_size) # unpad the padding added during encyption to produce the original message
+			
+			plaintext = plaintext.decode('utf-8') # convert from bytearray to utf8 encoded string
+
+			data = ["SUCCESS", plaintext]
+			data = json.dumps(data)
+
+			clientsocket.send(data.encode()) 
+			
+		except:
+			error_msg = "Error detected, please ensure that cipher entered and secret key are valid!"
+
+			data = ["FAIL", error_msg]
+
+			data = json.dumps(data)
+
+			clientsocket.send(data.encode())
+
+
+#================Server Starts Listening================#
+
+while True:
+
+	# wait until a client connects
+	(clientsocket, address) = serversocket.accept()
+
+	# call the "handle_client" function to interact with the client
+	handle_client(clientsocket, address)
+
+	# close the connection and start the next iteration of the loop to wait for the next client
+	clientsocket.close()
+
+
+print('Server is closing...')
+serversocket.close()
+
+
+
+'''
+Seqeunce of Events:
+1. Client connects to server
+2. Server accepts connection 
+
+3. Server asks if client wants to encrypt/ decrypt
+4. 	a. (Client wants to encrypt:)
+		- Client sends plaintext
+		- Server receives plaintext
+		- Client sends secret key
+		- Server receives secret key
+		- Server computes encryption
+		- Server sends ciphertext (in hex) to client 
+		- Client displays cipher and disconnects
+	b. (Client wants to decrypt)
+		- Client sends ciphertext (in hex)
+		- Server receives ciphertext
+		- Client sends secret key
+		- Server receives secret key
+		- Server decrypts
+		If decryption succesful, server sends to client, client dsiplays and disconnects
+		(if error, error message is displayed and then disconnects)
+'''
